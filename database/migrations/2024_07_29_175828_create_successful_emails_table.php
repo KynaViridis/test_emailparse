70e27c7e3720d0499d70edd1bd9a5cc25f242d58
Pedro Salgado <sgpedro95@gmail.com>
2024-07-30 10:59:11 -0300

    minor adjust

--- database/migrations/2024_07_29_175828_create_successful_emails_table.php
+++ database/migrations/2024_07_29_175828_create_successful_emails_table.php
@@ -13,23 +13,25 @@ class CreateSuccessfulEmailsTable extends Migration
      */
     public function up()
     {
-        Schema::create('successful_emails', function (Blueprint $table) {
-            $table->id();
-            $table->mediumInteger('affiliate_id');
-            $table->text('envelope');
-            $table->string('from', 255);
-            $table->text('subject');
-            $table->string('dkim', 255)->nullable();
-            $table->string('SPF', 255)->nullable();
-            $table->float('spam_score')->nullable();
-            $table->longText('email');
-            $table->longText('raw_text')->nullable();
-            $table->string('sender_ip', 50)->nullable();
-            $table->text('to');
-            $table->integer('timestamp');
-            $table->timestamps();
-            $table->softDeletes();
-        });
+        if (!Schema::hasTable('successful_emails')) {
+            Schema::create('successful_emails', function (Blueprint $table) {
+                $table->id();
+                $table->mediumInteger('affiliate_id');
+                $table->text('envelope');
+                $table->string('from', 255);
+                $table->text('subject');
+                $table->string('dkim', 255)->nullable();
+                $table->string('SPF', 255)->nullable();
+                $table->float('spam_score')->nullable();
+                $table->longText('email');
+                $table->longText('raw_text')->nullable();
+                $table->string('sender_ip', 50)->nullable();
+                $table->text('to');
+                $table->integer('timestamp');
+                $table->timestamps();
+                $table->softDeletes();
+            });
+        }
     }
 
     /**
